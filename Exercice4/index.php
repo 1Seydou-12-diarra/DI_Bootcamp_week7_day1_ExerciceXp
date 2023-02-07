76c228390f454dc5268096a7de7e17a443b2cc5e
SAID DIARRASSOUBA <seydou.diarrassouba@imako.digital>
2023-02-07 09:45:05 +0000

    update

--- Exercice4/index.php
+++ Exercice4/index.php
@@ -1,18 +1,20 @@
-<?php
-class Calculatrice {
-    // Méthode pour calculer la différence entre deux dates
-    public function difference($date1, $date2) {
-      $diff = strtotime($date2) - strtotime($date1);
-      return $diff / 86400;
-    }
-  }
 
-  // Instanciation de la classe Calculatrice
-  $calculatrice = new Calculatrice();
+<?php 
+//creation de la class DifferenceDate pour calculer la difference de deux date
+Class DifferenceDate{
+// les proprietés de la class
+  public $dt1 ;
+  public $dt2;
+//un function permetant de faire le calcul 
+  public function difference(){
+     $sdate = new DateTime("1981-11-03");
+     $edate = new DateTime("2013-09-04");
+     $interval = $sdate->diff($edate);
+     return $interval->y. " years, ". $interval->m." months, ".$interval->d." days ";
+}
+}
+// l'instanciation de l'objet $resultat et l'affichage du resultat 
+$resultat = new DifferenceDate();
+echo " Difference " . $resultat->difference();
 
-  // Appel de la méthode difference pour calculer la différence entre deux dates
-  $diff = $calculatrice->difference("2022-01-01", "2022-12-31");
-
-  // Affichage du résultat
-  echo "La différence entre les deux dates est : $diff jours";
 ?>
