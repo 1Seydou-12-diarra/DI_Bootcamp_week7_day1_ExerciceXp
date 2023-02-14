62e64777b74afa4db47b6165f5d9c8b8128a1005
SAID DIARRASSOUBA <seydou.diarrassouba@imako.digital>
2023-02-14 10:57:43 +0000

    update

--- exercice1/index.php
+++ exercice1/index.php
@@ -1,15 +1,44 @@
-<?php 
-//class Hello conteneant la proprieté $message et la function affichage() qui affiche un message  
-class Hello {
+--Create items table
+CREATE TABLE items (
+  item_id SERIAL PRIMARY KEY,
+  item_name VARCHAR(50) NOT NULL,
+  price INTEGER NOT NULL
+);
+--insert value into items
+INSERT INTO items (item_name, price)VALUES 
+('Small Desk', 100),
+('Large Desk', 300),
+('Fan', 80);
+--create customers table
+CREATE TABLE customers (
+  customer_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL
+);
+--insert into customers table
+INSERT INTO customers (first_name, last_name)
+VALUES 
+('Greg', 'Jones'),
+('Sandra', 'Jones'),
+('Scott', 'Scott'),
+('Trevor', 'Green'),
+('Melanie', 'Johnson');
 
-  public $message ;
 
-  function affichage($msg){
-    $this->message = $msg;
-    return $this->message;
-  }
+--  all the items
+SELECT * FROM items;
 
- }
-$bonjour = new  Hello();
-echo $bonjour->affichage('MyClass class has initialized !'."\n");
-?>
+--  all the items with a price above 80 (80 not included)
+SELECT * FROM items WHERE price > 80;
+
+--  all the items with a price below 300 (300 included)
+SELECT * FROM items WHERE price <= 300;
+
+--  all customers whose last name is ‘Smith’
+SELECT * FROM customers WHERE last_name = 'Smith';
+
+--  all customers whose last name is ‘Jones’
+SELECT * FROM customers WHERE last_name = 'Jones';
+
+--  all customers whose first name is not ‘Scott’
+SELECT * FROM customers WHERE first_name != 'Scott';
